822d0002d9284a4e8991f00a8bf3dd44834416ab
gabrielnascr <gr153tt@gmail.com>
2022-03-11 14:56:01 -0300

    add auth provider wrapper and add toast container

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+/* eslint-disable react/jsx-props-no-spreading */
+import { ToastContainer } from 'react-toastify';
+import { AuthProvider } from '../contexts/AuthContext';
 
+import '../styles/globals.css';
+import 'react-toastify/dist/ReactToastify.css';
+
+// eslint-disable-next-line react/prop-types
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+      <ToastContainer />
+    </AuthProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
